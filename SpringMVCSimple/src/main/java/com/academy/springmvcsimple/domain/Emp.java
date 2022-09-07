b1efc445a79df7ddb366b9cb621e1be505ebb7e3
dltmdcks98 <dltmdcks98@gmail.com>
2022-09-07 12:08:11 +0900

    22.09.07 DTO, DAO 생성

--- SpringMVCSimple/src/main/java/com/academy/springmvcsimple/domain/Emp.java
+++ SpringMVCSimple/src/main/java/com/academy/springmvcsimple/domain/Emp.java
@@ -6,10 +6,7 @@ import lombok.Data;
 public class Emp {
 	private int empno;
 	private int ename;
-	private String job;
-	private int mgr;
 	private String hiredate;
 	private int sal;
-	private int comm;
 	private Dept deptno;//숫자가 아니라, 객체를 has어 관계로 보유
 }
