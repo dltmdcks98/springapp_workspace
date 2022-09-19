de3ecfb3a58c8458427604bec34582a595ff927f
dltmdcks98 <dltmdcks98@gmail.com>
2022-09-19 12:18:23 +0900

    22.09.19 DTO, Mybatis Mapper생성

---
+++ Shopping/src/main/java/com/academy/shopping/model/domain/TopCategory.java
@@ -0,0 +1,10 @@
+package com.academy.shopping.model.domain;
+
+import lombok.Data;
+
+@Data
+public class TopCategory {
+	private int subcategory_id; 
+	private String category_name; 
+	private int topcategory_id;
+}
