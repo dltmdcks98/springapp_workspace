18351407bb0175bc636321fddde3ba5591a7054d
dltmdcks98 <dltmdcks98@gmail.com>
2022-09-21 10:22:59 +0900

    22.09.21 shop main,list include 및 main category 출력

--- Shopping/src/main/java/com/academy/shopping/controller/ShopMainController.java
+++ Shopping/src/main/java/com/academy/shopping/controller/ShopMainController.java
@@ -1,16 +1,32 @@
 package com.academy.shopping.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.academy.shopping.model.category.TopCategoryService;
 
 //쇼핑몰 메인데 대한 요청 처리
 @Controller
 public class ShopMainController {
 	
+	@Autowired
+	private TopCategoryService topCategoryService;
+	
+	
 	@GetMapping("/shop")
 	public ModelAndView getMain() {
+		//카테고리 가져오기 
+		List topCategoryList =topCategoryService.selectAll();
+		
+		//신상품 및 각종 기획 상품등 진열 
+		
 		ModelAndView mav = new ModelAndView("shop/index");
+		//카테고리 가져오기
+		mav.addObject("topCategoryList",topCategoryList);
 		return mav;
 	}
 	
